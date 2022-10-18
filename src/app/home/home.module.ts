9231f71beffa39550633d9e02eb3f6daaa92e231
Marius C. Bolik <marius@eyloo.com>
2022-10-18 11:58:48 +0200

    initial commit

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,16 +3,16 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
-
 import { HomePageRoutingModule } from './home-routing.module';
-
+import { NgxExtendedPdfViewerModule } from 'ngx-extended-pdf-viewer';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    NgxExtendedPdfViewerModule
   ],
   declarations: [HomePage]
 })
